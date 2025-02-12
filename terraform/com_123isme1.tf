14399ab4bb7b602aeeb7afc7981194da392ce12a
Bob Carlson <b0bcarlson@users.noreply.github.com>
2025-02-12 09:20:25 -0600

    Initial

---
+++ terraform/com_123isme1.tf
@@ -0,0 +1,31 @@
+resource "digitalocean_domain" "com_123isme1" {
+  name = "123isme1.com"
+}
+resource "digitalocean_record" "com_123isme1_a_root" {
+  domain = digitalocean_domain.com_123isme1.id
+  type   = "A"
+  name   = "@"
+  value  = local.ipAddress
+  ttl    = 300
+}
+resource "digitalocean_record" "com_123isme1_a_wild" {
+  domain = digitalocean_domain.com_123isme1.id
+  type   = "A"
+  name   = "*"
+  value  = local.ipAddress
+  ttl    = 300
+}
+resource "digitalocean_record" "com_123isme1_mx_10" {
+  domain   = digitalocean_domain.com_123isme1.id
+  type     = "MX"
+  name     = "@"
+  priority = 10
+  value    = "mxa.mailgun.org."
+}
+resource "digitalocean_record" "com_123isme1_mx_20" {
+  domain   = digitalocean_domain.com_123isme1.id
+  type     = "MX"
+  name     = "@"
+  priority = 20
+  value    = "mxb.mailgun.org."
+}
---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+locals {
+  ipAddress = "173.31.189.178"
+}
