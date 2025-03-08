bbd3edc58b82de52f95427ce2751d86e38f86472
Bob Carlson <b0bcarlson@users.noreply.github.com>
2025-03-08 19:31:46 +0000

    Fixes (#1)

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,22 @@ terraform {
     }
   }
 }
+provider "digitalocean" {}
+terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://s3.us-west-004.backblazeb2.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    region                      = "us-east-1"
+    bucket                      = "bobcodes-dns"
+    key                         = "terraform.tfstate"
+    skip_s3_checksum            = true
+  }
+}
 locals {
   ipAddress = "173.31.189.178"
 }
