47f48896bf93c528b1296aaad46f6b9e144c8663
Bob Carlson <b0bcarlson@users.noreply.github.com>
2025-03-08 12:52:21 -0600

    endpoints

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,13 @@ terraform {
 provider "digitalocean" {}
 terraform {
   backend "s3" {
+    endpoints = {
+      s3 = "https://s3.us-west-004.backblazeb2.com"
+    }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    endpoint                    = "https://s3.us-west-004.backblazeb2.com"
     region                      = "us-east-1"
     bucket                      = "bobcodes-dns"
     key                         = "terraform.tfstate"
