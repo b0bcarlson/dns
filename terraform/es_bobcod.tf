86992a0d1fb4e12acdda7367c459f1ce8e294157
Bob Carlson <b0bcarlson@users.noreply.github.com>
2025-03-09 08:46:29 -0500

    Remove acme challenge

--- terraform/es_bobcod.tf
+++ terraform/es_bobcod.tf
@@ -15,10 +15,3 @@ resource "digitalocean_record" "es_bobcod_a_wild" {
   value  = local.ipAddress
   ttl    = local.ttl
 }
-resource "digitalocean_record" "es_bobcod_txt_acme_challenge" {
-  domain = digitalocean_domain.es_bobcod.id
-  type   = "TXT"
-  name   = "_acme-challenge"
-  value  = "7rikilUWsv-5z6XS4x0nvDb5GBB_AJyHtkHVnaA58So"
-  ttl    = local.ttl
-}
