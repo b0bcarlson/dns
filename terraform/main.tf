0047d232ef197f504f0bb06407f3d95ac8f4d47e
Bob Carlson <b0bcarlson@users.noreply.github.com>
2025-05-20 10:30:55 -0500

    Placeholder acme challenges

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,6 @@ locals {
   ipAddress                         = "173.31.189.178"
   ttl                               = 300
   com_123isme1_acme_challenge_value = "DzTtUtp862VgssgsAvaeK8g6WODec3G-jGQOVVpKADU"
-  es_bobcod_acme_challenge_value    = ""
-  net_bobcodes_acme_challenge_value = ""
+  es_bobcod_acme_challenge_value    = "foo"
+  net_bobcodes_acme_challenge_value = "foo"
 }
